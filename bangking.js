ce5d8272e12a12ed17dddd1958f72c3bfa69460f
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-03-01 21:40:13 +0600

    deposit withdraw balance initiali done

--- bangking.js
+++ bangking.js
@@ -1,56 +1,61 @@
-// find element
-const depositeInput = document.getElementById('deposit-input')
-const depositBtn = document.getElementById('deposit-btn')
-const depositAmount = document.getElementById('deposit-amount')
-const depositTotalBalance = document.getElementById('deposit-total-balance')
+// deposit section
+const depositButton = document.getElementById('deposit-button');
+const depositInput = document.getElementById('deposit-input')
+const singleDeposit = document.getElementById('single-deposit')
 
-// withdraw
+// total balance find
+const depositBalance = document.getElementById('total-balance')
+
+// withdraw section
+const withdrawButton = document.getElementById('withdraw-button');
 const withdrawInput = document.getElementById('withdraw-input')
-const withdrawBtn = document.getElementById('withdraw-btn')
-const withdrawAmount = document.getElementById('withdraw-amount')
+const singleWithdraw = document.getElementById('single-withdraw')
 
-// deposite amount
-depositBtn.addEventListener('click', ()=>{
-    const previousDeposit = depositAmount.innerText;
-    const currentDeposit = depositeInput.value
-    const totalDeposite = parseFloat(previousDeposit) + parseFloat(currentDeposit);
+// deposit and total balance
+depositButton.addEventListener('click', ()=>{
+    const previousDeposit = singleDeposit.innerText;
+    const currentDeposit = depositInput.value;
+    const totalDeposit = parseFloat(previousDeposit) + parseFloat(currentDeposit)
     
-    if (isNaN(currentDeposit) || currentDeposit.trim() === '') {
-        alert('Invalid Input')
+    if( (isNaN(currentDeposit)) || currentDeposit.trim() === ''){
+        alert('Please give valid input')
         return;
     }
 
-    depositAmount.innerText = totalDeposite;
+    singleDeposit.innerText = totalDeposit;
 
-    // update total balance
-    const previousBalance = depositTotalBalance.innerText;
+
+    // total balance
+    const previousBalance = depositBalance.innerText;
     const totalBalance = parseFloat(previousBalance) + parseFloat(currentDeposit)
-    depositTotalBalance.innerText = totalBalance
-    depositeInput.value = ''
+    depositBalance.innerText = totalBalance;
+
+    depositInput.value = ''
 })
 
-
-// withdraw amount
-withdrawBtn.addEventListener('click', ()=>{
-    const previousWithdraw = withdrawAmount.innerText;
+// withdraw
+withdrawButton.addEventListener('click', ()=>{
+    const previousWithdraw = singleWithdraw.innerText;
     const currentWithdraw = withdrawInput.value;
-    const previousBalance = depositTotalBalance.innerText;
-    const totalBalance = parseFloat(previousBalance) - parseFloat(currentWithdraw)
-
-    if (isNaN(currentWithdraw) || currentWithdraw.trim() === '') {
-        alert('Invalid Input')
+    
+    if( (isNaN(currentWithdraw)) || currentWithdraw.trim() === ''){
+        alert('Please give valid input')
         return;
     }
     
-    if( totalBalance < 0 ){
+    const previousBalance = depositBalance.innerText;
+    const totalBalance = parseFloat(previousBalance) - parseFloat(currentWithdraw)
+
+    if( totalBalance < 0){
         alert('Insufficient Balance')
-        return withdrawAmount.innerText = previousWithdraw;
+        return;
     }else{
-         const totalWithdraw = parseFloat(previousWithdraw) + parseFloat(currentWithdraw);
-        withdrawAmount.innerText = totalWithdraw;
+        const totalWithdraw = parseFloat(previousWithdraw) + parseFloat(currentWithdraw)
+        singleWithdraw.innerText = totalWithdraw;
     }
-   
-    depositTotalBalance.innerText = totalBalance;
+
+    depositBalance.innerText = totalBalance;
+
     withdrawInput.value = ''
 })
 
