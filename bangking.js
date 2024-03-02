33116a23304f6466f907fa19d77fc061c02b7448
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-03-02 17:26:18 +0600

    change update

--- bangking.js
+++ bangking.js
@@ -13,30 +13,37 @@ const updateTotalField = (inputField,currentAmount) =>{
     const depositWithdraw = document.getElementById(inputField) 
     const previousAmount = depositWithdraw.innerText;
     const totalAmount = parseFloat(previousAmount) + parseFloat(currentAmount)
+
+    if(isNaN(currentAmount) || currentAmount.trim() === '' || currentAmount < 0){
+        alert('valid input')
+        return;
+    }
+
     depositWithdraw.innerText = totalAmount;
+}
+
+// update total balance
+const updateTotalBalance = (currentDeposit, isAdd) =>{
+    const previousTotal = depositTotalBalance.innerText;
+    if ( isAdd === true){
+        depositTotalBalance.innerText = parseFloat(previousTotal) + parseFloat(currentDeposit);
+    }else{
+        depositTotalBalance.innerText = parseFloat(previousTotal) - parseFloat(currentDeposit);
+    }
 }
 
 // deposit and total balance
 document.getElementById('deposit-button').addEventListener('click', ()=>{
     const currentDeposit = getInputValue('current-deposit')
     updateTotalField('previous-deposit',currentDeposit)
-
-// update total
-    const previousTotal = depositTotalBalance.innerText;
-    const totalBalance = parseFloat(previousTotal) + parseFloat(currentDeposit);
-    depositTotalBalance.innerText = totalBalance;
+    updateTotalBalance(currentDeposit, true)
 })
 
 // withdraw
 document.getElementById('withdraw-button').addEventListener('click', ()=>{
-    const withdrawText = document.getElementById('previous-withdraw')
     const currentWithdraw = getInputValue('current-withdraw')
     updateTotalField("previous-withdraw",currentWithdraw)
-
-    // update total
-    const previousTotal = depositTotalBalance.innerText;
-    const totalBalance = parseFloat(previousTotal) - parseFloat(currentWithdraw);
-    depositTotalBalance.innerText = totalBalance;
+    updateTotalBalance(currentWithdraw, false)
 })
 
 
