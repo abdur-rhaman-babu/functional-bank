266093cffb401ac7c805047fa957f6391ba8045d
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-03-02 16:35:38 +0600

    dynamic deposit withdraw

--- bangking.js
+++ bangking.js
@@ -1,64 +1,45 @@
-// total balance find
-const depositBalance = document.getElementById('total-balance')
-
-// InputField
-const getInputValue = (inputId)=>{
-    const inputField = document.getElementById(inputId)
+// getInputvalue
+const getInputValue = (inputId) =>{
+    const inputField = document.getElementById(inputId);
     const currentAmount = inputField.value;
-    inputField.value = ''
+    inputField.value = '';
     return currentAmount;
 }
 
-// innerText
-const getInputInnerText = (innerTextId)=>{
-    const singleAmount = document.getElementById(innerTextId)
-    return singleAmount;
+const depositTotalBalance = document.getElementById("total-balance")
+
+// updateTotalField
+const updateTotalField = (inputField,currentAmount) =>{
+    const depositWithdraw = document.getElementById(inputField) 
+    const previousAmount = depositWithdraw.innerText;
+    const totalAmount = parseFloat(previousAmount) + parseFloat(currentAmount)
+    depositWithdraw.innerText = totalAmount;
 }
 
 // deposit and total balance
 document.getElementById('deposit-button').addEventListener('click', ()=>{
-    const currentAmount = getInputValue('deposit-input');
-    const singleAmount = getInputInnerText('single-deposit')
+    const currentDeposit = getInputValue('current-deposit')
+    updateTotalField('previous-deposit',currentDeposit)
 
-    const previousDeposit = singleAmount.innerText;
-    const totalDeposit = parseFloat(previousDeposit) + parseFloat(currentAmount)
-    
-    if( (isNaN(currentAmount)) || currentAmount.trim() === '' || currentAmount < 0 ){
-        alert('Please give valid input')
-        return;
-    }
-
-    singleAmount.innerText = totalDeposit;
-
-    // total balance
-    const previousBalance = depositBalance.innerText;
-    const totalBalance = parseFloat(previousBalance) + parseFloat(currentAmount)
-    depositBalance.innerText = totalBalance;
-
+// update total
+    const previousTotal = depositTotalBalance.innerText;
+    const totalBalance = parseFloat(previousTotal) + parseFloat(currentDeposit);
+    depositTotalBalance.innerText = totalBalance;
 })
 
 // withdraw
 document.getElementById('withdraw-button').addEventListener('click', ()=>{
-    const currentAmount = getInputValue('withdraw-input');
-    const singleAmount = getInputInnerText('single-withdraw')
-    const previousWithdraw = singleAmount.innerText;
-    const totalWithdraw = parseFloat(previousWithdraw) + parseFloat(currentAmount)
-    
-    if( (isNaN(currentAmount)) || currentAmount.trim() === '' || currentAmount < 0 ){
-        alert('something is worng went')
-        return;
-    }
-    
-    const previousBalance = depositBalance.innerText;
-    const totalBalance = parseFloat(previousBalance) - parseFloat(currentAmount)
+    const withdrawText = document.getElementById('previous-withdraw')
+    const currentWithdraw = getInputValue('current-withdraw')
+    updateTotalField("previous-withdraw",currentWithdraw)
 
-    if( totalBalance < 0){
-        alert('something is worng went')
-        return;
-    }else{
-        singleAmount.innerText = totalWithdraw;
-    }
-
-    depositBalance.innerText = totalBalance;
+    // update total
+    const previousTotal = depositTotalBalance.innerText;
+    const totalBalance = parseFloat(previousTotal) - parseFloat(currentWithdraw);
+    depositTotalBalance.innerText = totalBalance;
 })
+
+
+
+
 
