43bedc313152d02a947762b0a1f11b416405cc8b
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-03-02 19:29:51 +0600

    fucntion bank done

--- bangking.js
+++ bangking.js
@@ -6,44 +6,49 @@ const getInputValue = (inputId) =>{
     return currentAmount;
 }
 
-const depositTotalBalance = document.getElementById("total-balance")
+const totalBalance = document.getElementById("total-balance")
 
 // updateTotalField
 const updateTotalField = (inputField,currentAmount) =>{
     const depositWithdraw = document.getElementById(inputField) 
     const previousAmount = depositWithdraw.innerText;
     const totalAmount = parseFloat(previousAmount) + parseFloat(currentAmount)
-
-    if(isNaN(currentAmount) || currentAmount.trim() === '' || currentAmount < 0){
-        alert('valid input')
-        return;
-    }
-
-    depositWithdraw.innerText = totalAmount;
+    depositWithdraw.innerText = totalAmount;  
 }
 
 // update total balance
-const updateTotalBalance = (currentDeposit, isAdd) =>{
-    const previousTotal = depositTotalBalance.innerText;
+const updateTotalBalance = (currentAmount, isAdd) =>{
+    const previousTotal = totalBalance.innerText;
+    
     if ( isAdd === true){
-        depositTotalBalance.innerText = parseFloat(previousTotal) + parseFloat(currentDeposit);
+        totalBalance.innerText = parseFloat(previousTotal) + parseFloat(currentAmount);
     }else{
-        depositTotalBalance.innerText = parseFloat(previousTotal) - parseFloat(currentDeposit);
+        totalBalance.innerText = parseFloat(previousTotal) - parseFloat(currentAmount);
     }
 }
 
 // deposit and total balance
 document.getElementById('deposit-button').addEventListener('click', ()=>{
     const currentDeposit = getInputValue('current-deposit')
-    updateTotalField('previous-deposit',currentDeposit)
-    updateTotalBalance(currentDeposit, true)
+    if ( currentDeposit > 0){
+        updateTotalField('previous-deposit',currentDeposit)
+        updateTotalBalance(currentDeposit, true)
+    }else{
+        alert('invalid input')
+    }
 })
 
 // withdraw
 document.getElementById('withdraw-button').addEventListener('click', ()=>{
     const currentWithdraw = getInputValue('current-withdraw')
-    updateTotalField("previous-withdraw",currentWithdraw)
-    updateTotalBalance(currentWithdraw, false)
+    const currentBalance = totalBalance.innerText;
+   
+    if( currentWithdraw > 0 && 0 < currentBalance){
+        updateTotalField("previous-withdraw",currentWithdraw)
+        updateTotalBalance(currentWithdraw, false)
+    }else{
+        alert('invalid input')
+    }
 })
 
 
